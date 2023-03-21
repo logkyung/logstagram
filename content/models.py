a11764ba2bfddef6c918e5aa458936b739bc54d1
logkyung <aebong54@gmail.com>
2023-03-21 13:37:31 +0900

    Update content/models.py and main.html

--- content/models.py
+++ content/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Feed(models.Model):
+    profile_image = models.TextField()
+    user_id = models.TextField()
+    image = models.TextField()
+    content = models.TextField()
+    like_count = models.IntegerField()
--- content/views.py
+++ content/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from .models import Feed
 
-# Create your views here.
+
+class Main(APIView):
+    def get(self, request):
+        feed_list = Feed.objects.all().order_by('-id')  # SELECT * FROM content_feed;
+
+        return render(request, 'logstagram/main.html', context={'feeds':feed_list})
