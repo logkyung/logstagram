48f8d6f2b3fa56009f8c37a1f0d5c64b215fa5ad
logkyung <aebong54@gmail.com>
2023-03-30 22:56:01 +0900

    Update signup api

--- user/views.py
+++ user/views.py
@@ -1,12 +1,36 @@
 from django.shortcuts import render
+from django.contrib.auth.hashers import make_password
+from rest_framework.response import Response
 from rest_framework.views import APIView
+from .models import User
 
 
 class SignupView(APIView):
     def get(self, request):
         return render(request, 'user/signup.html')
 
+    def post(self, request):
+        # TODO Signup
+        email = request.data.get('email', None)
+        name = request.data.get('name', None)
+        user_id = request.data.get('user_id', None)
+        password = request.data.get('password', None)
+
+        User.objects.create(
+            email=email,
+            name=name,
+            user_id=user_id,
+            password=make_password(password),
+            profile_image="default_profile.jpg",
+        )
+
+        return Response(status=200)
+
 
 class LoginView(APIView):
     def get(self, request):
         return render(request, 'user/login.html')
+
+    def post(self, request):
+        # TODO Login
+        pass
